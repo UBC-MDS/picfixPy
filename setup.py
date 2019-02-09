8949e2b20f332dfdaa214e10b82348ac11850306
mkeyim94 <milibankeyim@gmail.com>
2019-02-09 13:54:19 -0800

    added setup.py and updated .py at end of test

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='picfixPy',
+    version='0.1.0',
+    description='Make quick filter changes to your images',
+    author='Miliban Keyim, George J. J. Wu, Mani Kohli',
+    url='https://github.com/UBC-MDS/picfixPy',
+    packages=['picfixPy'],
+    license='LICENSE.md',
+    long_description=open('README.md').read(),
+)
