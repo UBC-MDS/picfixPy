ffc56ac790dd9823af1fa7ed25d44224c3039ddf
mkeyim94 <milibankeyim@gmail.com>
2019-02-13 20:50:03 -0800

    need to change, causing errors

--- picfixPy/test/test_vibrance.py
+++ picfixPy/test/test_vibrance.py
@@ -17,7 +17,7 @@ test_img1 = np.array([[[4,77,245], [44,44,32] ,[60,70,80]],
                       [[6,66,43], [33,55,63], [22,56,70]],
                       [[4,55,66], [65,77,43], [33,45,22]]], dtype = 'uint8')
 
-skimage.io.imsave("pixfixPy/test/test_img/vibrance/test_img1.png", test_img1)
+skimage.io.imsave("../picfixPy/test/test_img1.png", test_img1)
 
 # generate expected output image with intensity = 10
 
@@ -25,15 +25,18 @@ expected_img1 = np.array([[[ 100,  120,  140], [ 120,  140,  110], [ 140,  110, 
                          [[ 140,  180, 250], [ 180, 250,  140], [250,  140,  180]],
                          [[ 160, 220, 240], [220, 240,  160], [240,  160, 220]]], dtype = "uint8")
 
+
 def test_zero_intensity():
-    vibrance("pixfixPy/test/test_img/vibrance/test_img1.png", 
+    vibrance("../pixfixPy/test/test_img1.png",
              0,
-             "pixfixPy/test/test_img/vibrance/vibrance.png")
-    output_img = skimage.io.imread("pixfixPy/test/test_img/vibrance/vibrance.png")[:, :, :3]
+             "../pixfixPy/test/vibrance.png")
+
+    output_img = skimage.io.imread("../pixfixPy/test/vibrance.png")[:, :, :3]
     assert np.array_equal(output_img, test_img1), "Images should be indentical with 0 intensity."
 
+
 def test_correct_vibrance():
-    vibrance("pixfixPy/test/test_img/vibrance/test_img1.png", 
+    vibrance("pixfixPy/test/test_img/vibrance/test_img1.png",
              10,
              "pixfixPy/test/test_img/vibrance/expected_img1.png")
     output_img = skimage.io.imread("pixfixPy/test/test_img/vibrance/testImg1_output.png")[:, :, :3]
@@ -47,30 +50,30 @@ def test_input_string():
 
 def test_valid_intensity():
     with pytest.raises(AttributeError):
-        vibrance("pixfixPy/test/test_img/vibrance/test_img1.png", 
-                 -10.5, 
+        vibrance("pixfixPy/test/test_img/vibrance/test_img1.png",
+                 -10.5,
                 "pixfixPy/test/test_img/vibrance/vibrance.png")
 
 def test_input_nonimage():
     with pytest.raises(OSError):
-        vibrance("pixfixPy/test/test_img/vibrance/test_img1.R", 
-                 5, 
+        vibrance("pixfixPy/test/test_img/vibrance/test_img1.R",
+                 5,
                 "pixfixPy/test/test_img/vibrance/vibrance.png")
 
 def test_output_nonimage():
     with pytest.raises(OSError):
-        vibrance("pixfixPy/test/test_img/vibrance/test_img1.png", 
-                 5, 
+        vibrance("pixfixPy/test/test_img/vibrance/test_img1.png",
+                 5,
                 "pixfixPy/test/test_img/vibrance/vibrance.pdf")
 
 def test_input_exist():
     with pytest.raises(FileExistsError):
-        vibrance("pixfixPy/test/test_img/ffxiv/namazu.png", 
-                 5, 
+        vibrance("pixfixPy/test/test_img/ffxiv/namazu.png",
+                 5,
                 "pixfixPy/test/test_img/vibrance/vibrance.png")
 
 def test_output_path_valid():
     with pytest.raises(FileNotFoundError):
-        vibrance("pixfixPy/test/test_img/vibrance/test_img1.png", 
-                 5, 
-                "@( * O * )@")    
+        vibrance("pixfixPy/test/test_img/vibrance/test_img1.png",
+                 5,
+                "@( * O * )@")
