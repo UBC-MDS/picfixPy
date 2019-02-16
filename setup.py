f1b6a678827956adc8b1705db4325542fa32eb62
mkeyim94 <milibankeyim@gmail.com>
2019-02-16 11:16:36 -0800

    updated setup

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from distutils.core import setup
+import numpy as np
+from skimage.io import imread, imshow, imsave
+import matplotlib as matplotlib
+import colorsys
 
 setup(
     name='picfixPy',
-    version='0.1.0',
-    description='Make quick filter changes to your images',
+    version='0.2',
+    description='Add quick vibrance, contast, and sharpen filters to your images',
     author='Miliban Keyim, George J. J. Wu, Mani Kohli',
     author_email = 'milibankeyim@gmail.com, mskcan@gmail.com, georgewujia@gmail.com',
     url = 'https://github.com/UBC-MDS/picfixPy',
     packages=['picfixPy'],
     license='LICENSE.md',
-    long_description=open('README.md').read(),
+    long_description=open('README.txt').read(),
 )
