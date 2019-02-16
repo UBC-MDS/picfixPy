e8e38efb05d9afe3450be2a2b21d76f335e618fd
mani <mskcan@gmail.com>
2019-02-15 16:17:20 -0800

    updated sharpen code

--- picfixPy/sharpen.py
+++ picfixPy/sharpen.py
@@ -6,24 +6,24 @@ import skimage.io
 def sharpen(input_img, intensity=5, display=False, output_img=''):
     
     '''
-    Sharpens an image with given intensity specificication
+    Sharpens an image with given intensity specification
     
     
     Input
     ============
-    input_img -- string of path for an image file in .png format
+    input_img -- string of path for an image file in .jpg, .jpeg, .png format
     intensity -- integer in range [0,10] to specificy intensity of sharpening applied to the image,
                  defaults to 5.
     display -- bool. If True, output will be displayed. Defaults to False.
-    output_img -- string path for output .png image with adjusted sharpening 
+    output_img -- string path for output of corresponding .jpg, .jpeg, .png image with adjusted sharpening 
     
     Returns
     ============
-    an image file in .png format with adjusted sharpening intensity in specified output path
+    an image file in .jpg, .jpeg, .png format with adjusted sharpening intensity in specified output path
     
     Example
     ============
-    sharpen("../img/image.png", 5, display=False, "../img/sharpened_image.png")
+    sharpen("../img/image.jpg", 5, display=False, "../img/sharpened_image.jpg")
     '''
 
     # load input image
@@ -52,11 +52,14 @@ def sharpen(input_img, intensity=5, display=False, output_img=''):
         raise ValueError("Intensity level must be between 0 and 10.")
         
         
-    # blurring of image
+    # Guassian blurring of image
+    
     # approximation of Gaussian blur 3x3 kernel
+    # kernel attribution: https://en.wikipedia.org/wiki/Kernel_(image_processing)
     kernel = np.array([[1.0,2.0,1.0], [2.0,4.0,2.0], [1.0,2.0,1.0]])
     kernel = kernel / np.sum(kernel)
     
+    # attribution: https://bit.ly/2EbS4XD
     image_array = []
     for y in range(3):
         temp_array = np.copy(input_img)
@@ -70,18 +73,20 @@ def sharpen(input_img, intensity=5, display=False, output_img=''):
     image_array_sum = np.sum(image_array, axis=0)
     
     
-    # sharpening of image
-    threshold = 0
+    # sharpening of image by unsharp mask
     
+    # https://en.wikipedia.org/wiki/Unsharp_masking
+    # https://homepages.inf.ed.ac.uk/rbf/HIPR2/unsharp.htm
+    
+    # blurred image 
     blurred = image_array_sum
+    
+    # concept: sharpened = original + (original − blurred) × amount.
+    # attribution: https://bit.ly/2V3FAr0
     sharpened = float(intensity + 1) * input_img - float(intensity) * blurred
     sharpened = np.maximum(sharpened, np.zeros(sharpened.shape))
     sharpened = np.minimum(sharpened, 255 * np.ones(sharpened.shape))
     sharpened = sharpened.round().astype(np.uint8)
-    if threshold > 0:
-        low_contrast_mask = np.absolute(input_image - blurred) < threshold
-        np.copyto(sharpened, input_image, where=low_contrast_mask)
-    #return sharpened
     
     
     # display or save sharpened image
